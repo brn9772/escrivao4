239baf9563e5e5bd016ebc92b6ac387476723ffa
brn9772 <tintomax997720175@gmail.com>
2025-11-24 16:23:42 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+// Service Worker para PWA
+const CACHE_NAME = 'reunioes-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
